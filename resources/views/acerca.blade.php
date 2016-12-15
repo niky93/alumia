0d48bc8d9ed705a6b029d4bb072cf48b007a0d10
nikole <nicolericaldigmail.com>
2016-12-15 18:02:39 -0400

    nueva

---
+++ resources/views/acerca.blade.php
@@ -0,0 +1,23 @@
+@extends ('layouts.admin')
+@section ('contenido')
+<div class="row">
+	<div class="col-lg-8 col-md-8 col-sm-8 col-xs-12">
+		<h3>Acerca de...</h3>
+	</div>
+</div>
+
+<div class="row">
+<div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+	<table>
+		<tr><th>Estudiante:</th><td>Ely Nicole Ricaldi Arteaga</td></tr>
+		<tr><th>Email:</th><td>nicolericaldi@gmail.com</td></tr>
+		<tr><th>Empresa Responsable:</th><td> Metalvid </td></tr>
+		<tr><th>Docente:</th><td>Ing. Rolando Martinex</td></tr>
+		<tr><th>Materia:</th><td>Taller de Grado I</td></tr>
+		<tr><th>Acerca del proyecto:</th>  <td>Este proyecto lo hice con muchas ganas de asesinar a la persona con la que estaba trabajando ya que cada vez me cambiaba los requisitos y tenia que rehacer el software </td> </tr>
+		<tr><th>Partes del proyecto:</th>  <td>Este proyecto esta compuesto por dos partes una version movil y una version web</td> </tr>
+
+	</table>
+</div>
+</div>
+@endsection
